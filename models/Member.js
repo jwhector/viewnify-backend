bbff1538fd85a746741ae8b7a00c981012f20ba1
David Abbey <davidabbey@earthlink.net>
2021-12-02 13:38:40 -0800

    sharedcontroller added

--- models/Member.js
+++ models/Member.js
@@ -1,25 +1,25 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/sequelize');
 
-class Member extends Model {}
+class Member extends Model { }
 Member.init({
     id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true,
-      },
-      user_id:{
+    },
+    user_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         references: { model: 'user', key: 'id' }
     },
-    watchparty_id:{
+    watchparty_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         references: { model: 'watchparty', key: 'id' }
-    } 
-},{
+    }
+}, {
     sequelize,
     freezeTableName: true,
     underscored: true,
