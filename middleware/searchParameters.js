5a87890a5c7f28cc00443fa7783441761068d58a
David Abbey <davidabbey@earthlink.net>
2021-12-02 09:47:18 -0800

    morning pull

--- middleware/searchParameters.js
+++ middleware/searchParameters.js
@@ -7,6 +7,7 @@ const sequelize = require('../config/sequelize');
 async function searchAndFilter(user_id){
 
     const dislikes = await Dislike.findByPk(user_id)
+    console.log(dislikes)
 
     const searchResult = await 
 fetch('/tmdbSearch', {
